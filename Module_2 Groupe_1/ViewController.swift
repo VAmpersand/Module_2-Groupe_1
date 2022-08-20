f4f130e89a1917fcfefd16146f4867ad9d90b1c1
Viktor Prikolota <49166976+VAmpersand@users.noreply.github.com>
2022-08-20 13:55:09 +0300

    Initial Commit

---
+++ Module_2 Groupe_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Module_2 Groupe_1
+//
+//  Created by Viktor Prikolota on 20.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
