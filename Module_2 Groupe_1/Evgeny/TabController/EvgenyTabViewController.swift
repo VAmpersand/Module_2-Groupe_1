3d848a6ba942cdef10af8e2f327cdc11fdd80e63
NuTsOKoL <mi.sei@yandex.ru>
2022-08-28 12:07:15 +0300

    add Task4 compleat
    вроде что-то написал, но не могу проверить, как писал ранее не работает связь main
    пробовал исправить - не выходит

--- Module_2 Groupe_1/Evgeny/TabController/EvgenyTabViewController.swift
+++ Module_2 Groupe_1/Evgeny/TabController/EvgenyTabViewController.swift
@@ -14,7 +14,7 @@ final class EvgenyTabViewController: UIViewController {
     
     private let inCodeButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setTitle("InCode layout", for: .normal)
+        button.setTitle("Button", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = .red
         return button
@@ -22,11 +22,20 @@ final class EvgenyTabViewController: UIViewController {
     
     private let inCodeButton2: UIButton = {
         let button = UIButton(type: .system)
-        button.setTitle("InCode layout", for: .normal)
+        button.setTitle("Button2", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = .red
         return button
     }()
+    
+    private let inCodeButton3: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Button3", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .red
+        return button
+    }()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,9 +45,38 @@ final class EvgenyTabViewController: UIViewController {
         
         inCodeButton.translatesAutoresizingMaskIntoConstraints = false
         inCodeButton2.translatesAutoresizingMaskIntoConstraints = false
+        inCodeButton3.translatesAutoresizingMaskIntoConstraints = false
         
         
+        inCodeButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview().offset(100)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(116)
+            $0.width.equalToSuperview().offset(-20)
+        }
         
+        inCodeButton2.snp.makeConstraints {
+            $0.top.centerX.equalToSuperview()
+            $0.height.equalTo(50)
+            $0.width.equalTo(inCodeButton2.snp.height).multipliedBy(6)
+        }
+        
+        inCodeButton3.snp.makeConstraints {
+            $0.top.centerX.equalToSuperview()
+            $0.size.equalTo(inCodeButton2)
+        }
+        
+        
+        inCodeButton3.addTarget(self, action: #selector(buttonHundler), for: .touchUpInside)
+        
+        }
+        @objc func buttonHundler () {
+            print("buttonHundler")
+            
+            let controller  = InCodeLayoutViewController()
+            controller.modalPresentationStyle = .fullScreen
+            self.present(controller, animated: true)
+            
     }
 }
 
---
+++ Module_2 Groupe_1/Evgeny/TabController/inCodeLayoutViewController.swift
@@ -0,0 +1,94 @@
+//
+//  inCodeLayoutViewController.swift
+//  Module_2 Groupe_1
+//
+//  Created by mac on 28.08.2022.
+//
+
+import UIKit
+import SnapKit
+
+final class InCodeLayoutViewController: UIViewController {
+    
+    private let yellowView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .yellow
+        return view
+    }()
+    private let greenview: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green
+        return view
+    }()
+    private let redViewOne: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
+    private let redViewTwo: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
+    private let blueViewOne: UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        return view
+    }()
+    private let blueViewTwo: UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addSubview()
+        addConstraints()
+        configureAppearance()
+        }
+    private func addSubview() {
+        view.addSubview(yellowView)
+        yellowView.addSubview(greenview)
+        yellowView.addSubview(redViewOne)
+        yellowView.addSubview(redViewTwo)
+        redViewOne.addSubview(blueViewOne)
+        redViewTwo.addSubview(blueViewTwo)
+    }
+    private func addConstraints() {
+        yellowView.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(16)
+        }
+            
+        redViewOne.snp.makeConstraints {
+            $0.edges.top.left.right.equalTo(16)
+            $0.height.equalTo(redViewOne.snp.width)
+        }
+            
+        redViewTwo.snp.makeConstraints {
+            $0.bottom.left.right.equalTo(16)
+            $0.height.equalTo(redViewTwo.snp.width)
+        }
+            
+        greenview.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.height.equalTo(50)
+            $0.width.equalTo(greenview.snp.height).multipliedBy(6)
+        }
+        
+        blueViewOne.snp.makeConstraints {
+            $0.height.equalTo(50)
+            $0.width.equalTo(blueViewOne.snp.height)
+            $0.edges.equalTo(16)
+        }
+        blueViewTwo.snp.makeConstraints {
+            $0.height.equalTo(blueViewOne.snp.height)
+            $0.width.equalTo(blueViewOne.snp.width)
+            $0.bottom.right.equalTo(16)
+        }
+    }
+        private func configureAppearance() {
+            view.backgroundColor = .white
+        }
+}
