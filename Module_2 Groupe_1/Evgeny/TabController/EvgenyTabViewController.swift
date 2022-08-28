ea521289f8334c33a248526d9993819d80f1f64d
NuTsOKoL <mi.sei@yandex.ru>
2022-08-28 10:06:26 +0300

    ошибка main
    не могу исправить ошибку main, делал по инструкции из урока 4, но не получается..

--- Module_2 Groupe_1/Evgeny/TabController/EvgenyTabViewController.swift
+++ Module_2 Groupe_1/Evgeny/TabController/EvgenyTabViewController.swift
@@ -6,13 +6,39 @@
 //
 
 import UIKit
+import SnapKit
 
 final class EvgenyTabViewController: UIViewController {
 
+    @IBOutlet weak var intefaceBulderButton: UIButton!
+    
+    private let inCodeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("InCode layout", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .red
+        return button
+    }()
+    
+    private let inCodeButton2: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("InCode layout", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .red
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .blue
+        view.addSubview(intefaceBulderButton)
+        
+        inCodeButton.translatesAutoresizingMaskIntoConstraints = false
+        inCodeButton2.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        
     }
 }
 
